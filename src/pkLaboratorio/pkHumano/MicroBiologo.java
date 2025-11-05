cb33486739651a31eaac459909ab2dd4a8aa1c6e
quarks <slowedcode@gmail.com>
2025-11-05 13:33:05 -0500

    fix microbiologo

--- src/pkLaboratorio/pkHumano/MicroBiologo.java
+++ src/pkLaboratorio/pkHumano/MicroBiologo.java
@@ -25,22 +25,13 @@ public class MicroBiologo {
     public void observar(ReinoProtista protista) {
         System.out.println(nombre + " observa a " + protista.getNombre() + " bajo el microscopio.");
     }
-<<<<<<< HEAD
-
-    public String getLogin() {
-         return login; }
-    public String getPassword() { 
-        return password; }
-    public String getNombre() {
-         return nombre; }
-=======
     public void observarAlga(Alga alga) {
         System.out.println("El microbiólogo " + nombre + " está observando el alga:");
         alga.analizar();
     }
+    
     public String getLogin() { return login; }
     public String getPassword() { return password; }
     public String getNombre() { return nombre; }
->>>>>>> f1d2f0019fd36fbece2eb04e171ae6d4a21a7135
 }
 
